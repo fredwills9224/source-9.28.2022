19912d952a4516c981f13792eb4afb4d90b896f5
fredwills9224 <willsf9224@gmail.com>
2022-09-29 19:10:33 -0400

    hshs tpwd idb tsrf3 p

---
+++ src/user/UserRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const User = require('./User');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+router.post('/api/1.0/users', (req, res)=>{
+
+    bcrypt.hash(req.body.password, 10).then((hash)=>{
+
+        const user = {...req.body, password: hash};
+        User.create(user).then(()=>{
+            return res.send({ message: 'User created' });
+        });
+
+    });
+
+});
+
+module.exports = router;
