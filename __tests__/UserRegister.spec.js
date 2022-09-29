72a4322556bfc4cd931277a990f2c6e64e168992
fredwills9224 <willsf9224@gmail.com>
2022-09-29 04:55:45 -0400

    rtns scsmsg whn snupriv ts f

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -19,3 +19,22 @@ it('returns 200 OK when signup request is valid', (done)=>{
     ;
 
 });
+
+it('returns success message when signup requst is valid', (done)=>{
+
+    request(app)
+        .post('/api/1.0/users')
+        .send({
+
+            username: 'user1',
+            email:'user1@mail.com',
+            password: 'P4ssword'
+
+        })
+        .then((response)=>{
+            expect(response.body.message).toBe('User created');
+            done();
+        })
+    ;
+
+});
