fe310ead7cd43b04d8bd981f34760eeeff934843
fredwills9224 <willsf9224@gmail.com>
2022-09-29 12:02:38 -0400

    db init for mnul tstn

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-p4ss', {
     dialect: 'sqlite',
-    storage: './database.sqlite'
+    storage: './database.sqlite',
+    logging: false
 });
 
 module.exports = sequelize;
