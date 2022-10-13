cab8cea8c29b5a23f09a8f996b7fbd0ec3beb1e8
fredwills9224 <willsf9224@gmail.com>
2022-10-13 01:01:31 -0400

    validate username middleware refactor

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -2,20 +2,21 @@ const express = require('express');
 const UserService = require('./UserService');
 const router = express.Router();
 
-router.post('/api/1.0/users', (req, res, next)=>
-    {
-        
-        const user = req.body;
-        if (user.username === null){
-            return res.status(400).send({
-                validationErrors: {
-                    username: 'Username cannot be null' 
-                }
-            });
-        }
-        next();
+const validateUsername = (req, res, next)=>{
 
-    }, async (req, res)=>{
+    const user = req.body;
+    if (user.username === null){
+        return res.status(400).send({
+            validationErrors: {
+                username: 'Username cannot be null' 
+            }
+        });
+    }
+    next();
+
+};
+
+router.post('/api/1.0/users', validateUsername, async (req, res)=>{
 
     await UserService.save(req.body);
     return res.send({ message: 'User created' });
