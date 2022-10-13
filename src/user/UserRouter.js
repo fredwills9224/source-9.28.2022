a1fcf5077010d6c673f6645d2588aebbefa6baa4
fredwills9224 <willsf9224@gmail.com>
2022-10-12 22:58:09 -0400

    rtns usrnm cntbn whn usrnm in ts p

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -6,7 +6,11 @@ router.post('/api/1.0/users', async (req, res)=>{
 
     const user = req.body;
     if (user.username === null){
-        return res.status(400).send({validationErrors: {}});
+        return res.status(400).send({
+            validationErrors: {
+                username: 'Username cannot be null' 
+            }
+        });
     }
     await UserService.save(req.body);
     return res.send({ message: 'User created' });
