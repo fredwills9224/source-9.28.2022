6771ed2a07a013e86fdf682c3fbe61ea70240e8a
fredwills9224 <willsf9224@gmail.com>
2022-10-14 18:48:19 -0400

    rtns exptmsg whn fiv einv ts f

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -100,6 +100,7 @@ describe('User Registration', ()=>{
             ${'username'} | ${'usr'}          | ${'Must have min 4 and max 32 characters'}
             ${'username'} | ${'a'.repeat(33)} | ${'Must have min 4 and max 32 characters'}
             ${'email'}    | ${null}           | ${'E-mail cannot be null'}
+            ${'email'}    | ${'mail.com'}     | ${'E-mail is not valid'}
             ${'password'} | ${null}           | ${'Password cannot be null'}
         `('returns $expectedMessage when $field is $value', async ({field, expectedMessage, value})=>{
 
