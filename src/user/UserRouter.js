bf22b1f88526ade04014de268ce34472892ff15d
fredwills9224 <willsf9224@gmail.com>
2022-10-13 23:41:47 -0400

    rtns ersfb whn usrnm + ein tsrf 1/10 p

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,45 +1,49 @@
 const express = require('express');
 const UserService = require('./UserService');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
-const validateUsername = (req, res, next)=>{
+// const validateUsername = (req, res, next)=>{
 
-    const user = req.body;
-    if (user.username === null){
+//     const user = req.body;
+//     if (user.username === null){
         
-        req.validationErrors = {
-            username: 'Username cannot be null' 
-        };
+//         req.validationErrors = {
+//             username: 'Username cannot be null' 
+//         };
 
-    }
-    next();
+//     }
+//     next();
 
-};
-const validateEmail = (req, res, next)=>{
+// };
+// const validateEmail = (req, res, next)=>{
 
-    const user = req.body;
-    if(user.email === null){
+//     const user = req.body;
+//     if(user.email === null){
 
-        req.validationErrors = {
-            ...req.validationErrors,
-            email: 'E-mail cannot be null'
-        };
+//         req.validationErrors = {
+//             ...req.validationErrors,
+//             email: 'E-mail cannot be null'
+//         };
 
-    }
-    next();
+//     }
+//     next();
 
-};
+// };
 
-router.post('/api/1.0/users', validateUsername, validateEmail, async (req, res)=>{
+router.post('/api/1.0/users', 
+    check('username').notEmpty(),
+    check('email').notEmpty(), async (req, res)=>{
 
-    if(req.validationErrors){
+    const errors = validationResult(req);
+    if(errors){
 
-        const response = { 
-            validationErrors: { 
-                ...req.validationErrors 
-            } 
-        };
-        return res.status(400).send(response);
+        // const response = { 
+        //     validationErrors: { 
+        //         ...req.validationErrors 
+        //     } 
+        // };
+        return res.status(400).send(errors);
     
     }
     await UserService.save(req.body);
