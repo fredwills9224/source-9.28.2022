76f65573bb1ac996009309ff4301e5c3c2ed3119
fredwills9224 <willsf9224@gmail.com>
2022-10-21 18:46:49 -0400

    frc tru clr init db em unq 2 usr ts f

--- src/user/User.js
+++ src/user/User.js
@@ -11,7 +11,8 @@ User.init(
             type: Sequelize.STRING
         },
         email: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            unique: true
         },
         password: {
             type: Sequelize.STRING
