61097f44239991bc438881afa1e9cf8c97fd342f
fredwills9224 <willsf9224@gmail.com>
2022-10-25 12:40:44 -0400

    internationaliztion validationErrors ts p

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -5,25 +5,25 @@ const {check, validationResult} = require('express-validator');
 
 router.post('/api/1.0/users', 
         check('username')
-            .notEmpty().withMessage('Username cannot be null').bail()
-            .isLength({min: 4, max: 32}).withMessage('Must have min 4 and max 32 characters')
+            .notEmpty().withMessage('username_null').bail()
+            .isLength({min: 4, max: 32}).withMessage('username_size')
         ,
         check('email')
-            .notEmpty().withMessage('E-mail cannot be null').bail()
-            .isEmail().withMessage('E-mail is not valid').bail()
+            .notEmpty().withMessage('email_null').bail()
+            .isEmail().withMessage('email_invalid').bail()
             .custom( async (email)=> {
 
                 const user = await UserService.findByEmail(email);
                 if(user){
-                    throw new Error('E-mail in use');
+                    throw new Error('email_inuse');
                 }
 
             })
         ,
         check('password')
-            .notEmpty().withMessage('Password cannot be null').bail()
-            .isLength({min: 6}).withMessage('Password must be at least 6 characters').bail()
-            .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/).withMessage('Password must have at least 1 uppercase, 1 lowercase letter and 1 number')
+            .notEmpty().withMessage('password_null').bail()
+            .isLength({min: 6}).withMessage('password_size').bail()
+            .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/).withMessage('password_pattern')
         , 
     async (req, res)=>{
 
@@ -31,7 +31,7 @@ router.post('/api/1.0/users',
     if(!errors.isEmpty()){
 
         const validationErrors = {};
-        errors.array().forEach( error => ( validationErrors[error.param] = error.msg ));
+        errors.array().forEach( error => ( validationErrors[error.param] = req.t(error.msg)));
         return res.status(400).send({ validationErrors: validationErrors });
     
     }
